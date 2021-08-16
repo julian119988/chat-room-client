5d3bdd07e8a46f14aed25f5f2becbd5940d8c142
Julian Zambroni <julian.zambroni@gmail.com>
2021-08-16 20:51:38 -0300

    Cambio url otra vez

--- src/App.js
+++ src/App.js
@@ -4,40 +4,40 @@ import Main from "./Components/Main/Main";
 import Index from "./Components/Index/Index";
 import "./App.scss";
 
-const SERVER = "https://simple-chat-appz.herokuapp.com/";
+const SERVER = "https://chat-room-app-weld.vercel.app/";
 
 function App() {
-  const [user, setUser] = useState();
-  const [socket, setSocket] = useState();
+    const [user, setUser] = useState();
+    const [socket, setSocket] = useState();
 
-  useEffect(() => {
-    const io = socketIOClient(SERVER);
-    setSocket(io);
-  }, []);
-  function getUser(username) {
-    setUser({
-      username: username,
-    });
-  }
+    useEffect(() => {
+        const io = socketIOClient(SERVER);
+        setSocket(io);
+    }, []);
+    function getUser(username) {
+        setUser({
+            username: username,
+        });
+    }
 
-  useEffect(() => {
-    if (user) {
-      socket.emit("usuarioEstablecido", {
-        usuario: user.username,
-        user_id: socket.id,
-      });
-    } // eslint-disable-next-line
-  }, [user]); // eslint-disable-next-line
+    useEffect(() => {
+        if (user) {
+            socket.emit("usuarioEstablecido", {
+                usuario: user.username,
+                user_id: socket.id,
+            });
+        } // eslint-disable-next-line
+    }, [user]); // eslint-disable-next-line
 
-  return (
-    <>
-      {user ? (
-        <Main socket={socket} user={user} />
-      ) : (
-        <Index getUser={getUser} />
-      )}
-    </>
-  );
+    return (
+        <>
+            {user ? (
+                <Main socket={socket} user={user} />
+            ) : (
+                <Index getUser={getUser} />
+            )}
+        </>
+    );
 }
 
 export default App;
--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,6 +1,6 @@
 import "./Title.scss";
 export const Title = () => (
   <div className="title">
-    <h2>Zambroni's chat app</h2>
+    <h2>Simple chat app</h2>
   </div>
 );
